db1f203f8db889a138377b2a0f03ff4cbc413b76
Lucas Lopes <l.lopes.fga@gmail.com>
2021-05-26 08:13:56 -0300

    feat: descrição da api

--- buscacep/src/main/java/com/example/buscacep/config/SpringfoxConfig.java
+++ buscacep/src/main/java/com/example/buscacep/config/SpringfoxConfig.java
@@ -1,22 +1,38 @@
 package com.example.buscacep.config;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
-public class SpringfoxConfig {    
+public class SpringfoxConfig {
 
     @Bean
     public Docket api() { 
-        return new Docket(DocumentationType.SWAGGER_2)  
-          .select()                                  
-          .apis(RequestHandlerSelectors.any())              
-          .paths(PathSelectors.any())                          
-          .build();                                           
+        return new Docket(DocumentationType.SWAGGER_2)                                 
+            .select()                                       
+            .apis(RequestHandlerSelectors.basePackage("com.example"))
+            .paths(PathSelectors.any())                     
+            .build()
+            .apiInfo(apiInfo());                                          
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+          "Busca CEP API", 
+          "Serviço de busca de CEP", 
+          "", 
+          "", 
+          null, 
+          "",
+          "",
+          Collections.emptyList());
     }
 }
