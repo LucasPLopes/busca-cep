923877bbd3daf27e60ffadf8516a59f0a4e259bd
Lucas Lopes <l.lopes.fga@gmail.com>
2021-06-01 09:01:54 -0300

    fix: corrige implementação para buscar cep substituindo a direita

--- buscacep/src/main/java/com/example/buscacep/service/CepService.java
+++ buscacep/src/main/java/com/example/buscacep/service/CepService.java
@@ -15,35 +15,67 @@ import reactor.core.publisher.Mono;
 @Slf4j
 public class CepService {
 
+    private static final int tamanho = 8;
 
     @Autowired
     private ExternalClient externalClient;
 
-    public Mono<Cep> busca(String cep){
+    public Mono<Cep> busca(String cep) {
         log.info("service: buscar cep {}", cep);
-
-        String cepCompletado = this.completaCep(cep);
-        log.info("service: cep completado {}", cep);
-        
-        Mono<Cep> retornado =  this.externalClient.getInstance().get().uri("/{cep}/json",cepCompletado).retrieve().bodyToMono(Cep.class);
-
-        if(retornado.block().getCep() == null)
-            throw new  CepException(ExceptionMessage.CEP_NAO_ENCONTRADO);
+        Mono<Cep> retornado = continuaBusca(cep);
+        verificaCepNaoEncontrado(retornado);
 
         return retornado;
     }
 
-    public String completaCep(String cep){
-        int tamanho = 8;
-        if(cep.trim().length() == tamanho)
-            return cep;
+    public String completaCep(String cep) {
+        return completaComZeros(cep) + cep;
+    }
+
+    public String completaCepADireita(String cep) {
+        return cep + completaComZeros(cep);
+    }
+
+    public Mono<Cep> continuaBusca(String cep) {
+        Mono<Cep> retornado = null;
+        for (int i = 0; i <= cep.length(); i++) {
+            String cepNovo = cep.substring(0, cep.length() - i);
+            String cepCompletado = completaCepADireita(cepNovo);
+            log.info("service: continua busca {}", cepCompletado);
+            if (cepCompletado == null) {
+                break;
+            }
+            Mono<Cep> encontrado = this.buscaCep(cepCompletado);
+            if (encontrado.block().getCep() != null) {
+                retornado = encontrado;
+                break;
+            }
+        }
+
+        return retornado;
+    }
+
+    private String completaComZeros(String cep) {
+        if (cep.trim().length() == tamanho)
+            return "";
         int diferenca = tamanho - cep.length();
         String zeros = "";
         for (int i = 0; i < diferenca; i++) {
-            zeros +="0";
+            zeros += "0";
         }
+        return zeros;
+    }
 
-        return zeros + cep;
+    private Mono<Cep> buscaCep(String cepCompletado) {
+        return this.externalClient.getInstance().get().uri("/{cep}/json", cepCompletado).retrieve()
+                .bodyToMono(Cep.class);
+    }
+
+    private void verificaCepNaoEncontrado(Mono<Cep> retornado) {
+        if (retornado == null)
+            throw new CepException(ExceptionMessage.CEP_NAO_ENCONTRADO);
+        if (retornado.block().getCep() == null)
+            throw new CepException(ExceptionMessage.CEP_NAO_ENCONTRADO);
     }
 
 }
